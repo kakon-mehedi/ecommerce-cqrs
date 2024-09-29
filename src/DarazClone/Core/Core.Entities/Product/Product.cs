f5386d5a41ac66d49adba29acd3b8dfde859fa04
kakon-selise <kakon.mehedi@selise.ch>
2024-09-29 18:31:14 +0600

    feat: Add default value to the entity

--- src/DarazClone/Core/Core.Entities/Product/Product.cs
+++ src/DarazClone/Core/Core.Entities/Product/Product.cs
@@ -6,26 +6,26 @@ namespace DarazClone.Core.Entities.Product;
 public class Product : EntityBase
 {
     public ObjectId Id { get; set; }
-    public string Name { get; set; }
-    public decimal Price { get; set; }
-    public string Category { get; set; }
-    public string Type { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal Price { get; set; } = decimal.Zero;
+    public string Category { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
 }
 
 public class ElectronicsProduct : Product
 {
-    public string ScreenSize { get; set; }
-    public string BatteryCapacity { get; set; }
+    public string ScreenSize { get; set; } = string.Empty;
+    public string BatteryCapacity { get; set; } = string.Empty;
 }
 
 public class ClothingProduct : Product
 {
-    public string Size { get; set; }
-    public string Material { get; set; }
+    public string Size { get; set; } = string.Empty;
+    public string Material { get; set; } = string.Empty;
 }
 
 public class BookProduct : Product
 {
-    public string Author { get; set; }
-    public int PageCount { get; set; }
+    public string Author { get; set; } = string.Empty;
+    public int PageCount { get; set; } = 0;
 }
